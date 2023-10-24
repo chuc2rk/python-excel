94ab86ce458ccc981bf84e5226d175f534ee4182
chuc2rk <chuc2rk@gmail.com>
2023-10-24 15:18:26 +0700

    chatgpt edit assignment

--- Pandas_assignment_chatpgt.py
+++ Pandas_assignment_chatpgt.py
@@ -17,6 +17,11 @@ data["month_year"] = data["Order Date"].dt.strftime("%m-%Y")
 grouped_data = data.groupby("month_year")
 
 # Iterate through the groups and save each group to a separate Excel file
+# 'group_name' is the name of the group (e.g., the value in the "month_year" column)
+# 'group_data' is the DataFrame containing the data for that group
+# This would iterate through each group (grouped by the values in the "month_year" column) 
+# and provide you with both the group name (e.g., "03-2022") and the associated DataFrame
+# containing data for that group.
 for group_name, group_data in grouped_data:
     # Generate the file name based on the group name
     file_name = f"{group_name}.xlsx"
